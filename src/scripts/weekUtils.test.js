a4a48008db9188ad787f7aa141a6113a301575b7
Joonas <jopotthast@gmail.com>
2024-11-12 20:03:08 +0100

    add options for testing

--- src/scripts/weekUtils.test.js
+++ src/scripts/weekUtils.test.js
@@ -1,48 +1,47 @@
-import { createWeekDict, getWeeksFromMonth } from './weekUtils.js';
+import { createWeekDict, getWeeksFromMonth } from './weekUtils'
 
 const monthData = [
   {
-    "id": "ID-60hl2lfv73s",
-    "day": "2024-02-04T23:00:00.000Z",
-    "week": 6,
-    "month": 2,
-    "quarter": 1,
-    "year": 2024,
-    "annotations": []
+    id: 'ID-60hl2lfv73s',
+    day: '2024-02-04T23:00:00.000Z',
+    week: 6,
+    month: 2,
+    quarter: 1,
+    year: 2024,
+    annotations: [],
   },
   {
-    "id": "ID-brdtmx31qb",
-    "day": "2024-02-13T23:00:00.000Z",
-    "week": 7,
-    "month": 2,
-    "quarter": 1,
-    "year": 2024,
-    "annotations": [
+    id: 'ID-brdtmx31qb',
+    day: '2024-02-13T23:00:00.000Z',
+    week: 7,
+    month: 2,
+    quarter: 1,
+    year: 2024,
+    annotations: [
       {
-        "name": "Type-hfa9hve7qd",
-        "color": "hsl(220deg, 100%, 90%)"
+        name: 'Type-hfa9hve7qd',
+        color: 'hsl(220deg, 100%, 90%)',
       },
       {
-        "name": "Type-rjqylza1kpn",
-        "color": "hsl(215deg, 100%, 90%)"
-      }
-    ]
-  }
-];
-
+        name: 'Type-rjqylza1kpn',
+        color: 'hsl(215deg, 100%, 90%)',
+      },
+    ],
+  },
+]
 
 describe('weekUtils functions', () => {
   it('should create a weekDict based on monthData', () => {
-    const result = createWeekDict(monthData);
+    const result = createWeekDict(monthData)
     console.log(result)
-    expect(result);
-  });
-});
+    expect(result)
+  })
+})
 
 describe('weekUtils functions', () => {
   it('should give all weeks from month', () => {
-    const result = getWeeksFromMonth(monthData[0]?.day);
+    const result = getWeeksFromMonth(monthData[0]?.day)
     console.log(result)
-    expect(result.length).toBe(5);
-  });
-});
+    expect(result.length).toBe(5)
+  })
+})
